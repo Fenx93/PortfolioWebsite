68ff1f7bbdf64d998b3f43564eee7b26705931af
david_smirnov <davidsmidik@gmail.com>
2021-03-15 13:21:57 +0000

    Edited project pages, added highlights, changed navbar on main, added background images to projects

---
+++ script.js
@@ -0,0 +1,17 @@
+var coll = document.getElementsByClassName("dropdown-header");
+var backgroundImage = document.getElementsByClassName("background");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      backgroundImage.style.height = "120%";
+    } else {
+      content.style.display = "block";
+      backgroundImage.style.height = "140%";
+    }
+  });
+}
